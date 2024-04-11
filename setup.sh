eb2b47e2b0a5900cf5d797afa3972495048cce39
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-04-11 11:56:01 +0100

    Username update
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 # FMAP Experiments
 # A collection of experiments to test the FMAP audio file format.
-# GitHub: https://www.github.com/lewisevans2007/FMAP_Experiments
+# GitHub: https://www.github.com/0x4248/FMAP_Experiments
 # License: GNU General Public License v3.0
-# By: Lewis Evans
+# By: 0x4248
 
-git clone https://www.github.com/lewisevans2007/FMAP FMAP-git
+git clone https://www.github.com/0x4248/FMAP FMAP-git
 mv FMAP-git/FMAP FMAP
 rm -rf FMAP-git
--- src/equations.py
+++ src/equations.py
@@ -1,8 +1,8 @@
 # FMAP Experiments
 # A collection of experiments to test the FMAP audio file format.
-# GitHub: https://www.github.com/lewisevans2007/FMAP_Experiments
+# GitHub: https://www.github.com/0x4248/FMAP_Experiments
 # License: GNU General Public License v3.0
-# By: Lewis Evans
+# By: 0x4248
 
 import math
 import matplotlib.pyplot
